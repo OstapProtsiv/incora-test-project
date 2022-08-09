815dad45c273d05fb038fd6d202daa3b1152a0d7
Prosiv Ostap <protsiv.ost@gmail.com>
2022-08-09 20:44:32 +0300

    fixed last_name.validator

--- middleware/last_name.validator.js
+++ middleware/last_name.validator.js
@@ -3,15 +3,16 @@ module.exports = (req, res, next) => {
     const { last_name } = req.body;
     if (!last_name) {
       next();
-    }
-    if (typeof last_name !== 'string') {
-      if (req.errors) {
-        req.errors.push({ param: 'last_name', msg: 'must be string' });
-      } else {
-        req.errors = [{ param: 'last_name', msg: 'must be string' }];
+    } else {
+      if (typeof last_name !== 'string') {
+        if (req.errors) {
+          req.errors.push({ param: 'last_name', msg: 'must be string' });
+        } else {
+          req.errors = [{ param: 'last_name', msg: 'must be string' }];
+        }
       }
+      next();
     }
-    next();
   } catch (error) {
     console.log(error);
     next(error);
